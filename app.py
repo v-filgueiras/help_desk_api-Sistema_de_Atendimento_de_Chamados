96c83a1699c6f5700ab8bab8ed61b92dec07629f
Victor-Filgueiras <vfilgueiras.dev@gmail.com>
2026-01-27 15:12:25 -0400

    Fix client routes and add update/delete functionality

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def create_client():
                         name= data_client["name"],
                         email= data_client["email"],
                         age= data_client["age"],
-                        active= True)
+                        active= data_client["active"])
     
     client_list.append(new_client.to_dict())
     client_id = client_id + 1
@@ -35,7 +35,7 @@ def get_clients():
     ), 200
 
 
-@app.route("/clients/<int:id>", methods=['GET'])
+@app.route("/client/<int:id>", methods=['GET'])
 def get_client(id):
     for client in client_list:
         if client["client_id"] == id:
@@ -44,6 +44,39 @@ def get_client(id):
     return jsonify({
         "message": "client not found"
     }), 404
+
+
+@app.route("/client/<int:id>", methods=['PUT'])
+def update_client(id):
+    data_client = request.get_json()
+
+    for client in client_list:
+        if client["client_id"] == id:
+            client["name"] = data_client["name"]
+            client["email"] = data_client["email"]
+            client["age"] = data_client["age"]
+            client["active"] = data_client["active"]
+
+            return jsonify(
+                {
+                    "message": "client updated",
+                    "client": client
+                }
+            ), 200
+
+    return jsonify({"message": "client not found"}), 404
+
+    
+@app.route("/client/<int:id>", methods=['DELETE'])
+def delete_client(id):
+    for client in client_list:
+        if client["client_id"] == id:
+            client_list.remove(client)
+            print(client_list)
+    
+    return jsonify (
+        {"message": "client deleted"}
+        ) , 200
 
 
 if __name__ == "__main__":
